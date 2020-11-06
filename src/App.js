a470653554972fa4908495b13f763423c88e4d09
AlliageSphere <alliagesphere@live.fr>
2020-11-06 19:15:39 +0100

    Projet complet formulaire

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+	state = {
+		inputClient: "",
+		inputId: 1,
+		clients: [
+		]
+	};
+	
+	
+	
+	handleInputChange = event => {
+		console.log("Entrée modifiée.");
+		this.setState({inputClient: event.target.value});
+	}
+	
+	handleButton = () => {
+		console.log("Bouton cliqué");
+		
+		const newClients = this.state.clients.slice(); //Copie le tableau.
+		newClients.push({id: this.state.inputId, nom: this.state.inputClient});
+		this.setState({inputId: this.state.inputId + 1, clients: newClients});
+		
+		console.log(this.state.inputId);
+	}
+	
+	handleDelete = id => {
+		console.log(id);
+		
+		const newClients = this.state.clients.slice();
+		const index = newClients.findIndex(function (client) {
+			return client.id === id;
+		});
+		
+		newClients.splice(index, 1);
+		
+		this.setState({clients: newClients});
+	}
+	
+	render()
+	{
+		const title = "Liste des clients";
+		
+		return(
+			<div>
+				<h1>{title}</h1>
+				
+				<ul>
+					{
+					this.state.clients.map(client => (<li>{client.nom} <button value={client.id} onClick={() => this.handleDelete(client.id)}>X</button></li>))
+					}
+				</ul>
+				
+				<div>
+					<input type="text" placeholder="Ajouter un client" onChange={this.handleInputChange}/>
+					<button onClick={this.handleButton}>Confirmer</button>
+				</div>
+			</div>
+		);
+		
+	}
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
